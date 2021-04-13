e7f7e7340f405d378248976c6341e60c20b33fea
unknown <rezakhakpoorboraz@gmail.com>
2021-04-13 10:02:38 -0700

    bookcategory-and-member-services-unit-tests-passed

---
+++ test/Library.Services.Tests.Unit/BookCategories/BookCategoryServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Library.Infrastructure.Application;
+using Library.Infrastructure.Test;
+using Library.Persistence.EF;
+using Library.Persistence.EF.BookCategories;
+using Library.Services.BookCategories;
+using Library.Services.BookCategories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Library.Services.Tests.Unit.BookCategories
+{
+    public class BookCategoryServiceTests
+    {
+        private BookCategoryService sut;
+        private BookCategoryRepository _bookCategoryRepository;
+        private UnitOfWork _unitOfWork;
+        private EFDataContext context;
+        private EFDataContext readContext;
+        public BookCategoryServiceTests()
+        {
+            var db = new EFInMemoryDatabase();
+            context = db.CreateDataContext<EFDataContext>();
+            readContext = db.CreateDataContext<EFDataContext>();
+            _bookCategoryRepository = new EFBookCategoryRepository(context);
+            _unitOfWork = new EFUnitOfWork(context);
+            sut = new BookCategoryAppService(_bookCategoryRepository,_unitOfWork);
+        }
+        [Fact]
+        public void Add_add_book_category_properly()
+        {
+            //Arrange
+            AddBookCategoryDto dto = new AddBookCategoryDto() { 
+                Title = "dummy-title",
+            };
+
+            //Act
+            var actualReturnedId = sut.Add(dto);
+
+            //Assert
+            var expected = readContext.BookCategories.Single(_ => _.Id == actualReturnedId);
+            expected.Title.Should().Be("dummy-title");
+        }
+    }
+}
---
+++ test/Library.Services.Tests.Unit/Members/MemberServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Library.Infrastructure.Application;
+using Library.Infrastructure.Test;
+using Library.Persistence.EF;
+using Library.Persistence.EF.Members;
+using Library.Services.Members;
+using Library.Services.Members.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Library.Services.Tests.Unit.Members
+{
+    public class MemberServiceTests
+    {
+        private MemberService sut;
+        private MemberRepository _memberRepository;
+        private UnitOfWork _unitOfWork;
+        private EFDataContext context;
+        private EFDataContext readContext;
+        public MemberServiceTests()
+        {
+            var db = new EFInMemoryDatabase();
+            context = db.CreateDataContext<EFDataContext>();
+            readContext = db.CreateDataContext<EFDataContext>();
+            _memberRepository = new EFMemberRepository(context);
+            _unitOfWork = new EFUnitOfWork(context);
+            sut = new MemberAppService(_memberRepository, _unitOfWork);
+        }
+        [Fact]
+        public void Add_add_member_properly()
+        {
+            //Arrange
+            AddMemberDto dto = new AddMemberDto()
+            {
+                Fullname = "dummy-name",
+                Address = "dummy-address",
+                Age = 1
+            };
+
+            //Act
+            var actualReturnedId = sut.Add(dto);
+
+            //Assert
+            var expected = readContext.Members.Single(_ => _.Id == actualReturnedId);
+            expected.Age.Should().Be(1);
+            expected.Address.Should().Be("dummy-address");
+            expected.Fullname.Should().Be("dummy-name");
+        }
+    }
+}
